1ca8d39703e62d204431344e6a20c9049b2a5dba
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-02-28 22:26:01 +0300

    -

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"net/http"
 
@@ -31,11 +32,21 @@ func search(x *gin.Context) {
 	x.Request.ParseForm()
 	log.Println(x.Request.Form)
 	data := x.Request.FormValue("City")
+	getData()
 
 	varToPass := gin.H{
 		"City": data,
 	}
 
 	x.HTML(http.StatusOK, "results.html", varToPass)
+
+}
+func getData(x *gin.Context) (r *http.Request) {
+	posturl := "https://jsonplaceholder.typicode.com/posts"
+	r, err := http.NewRequest("POST", posturl, bytes.NewBuffer(body))
+	if err != nil {
+		panic(err)
+	}
+	return r
 
 }
